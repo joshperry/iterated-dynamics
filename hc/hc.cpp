b5aff509d494bf0dfca16ffd52c9a5b57eb3206c
Richard <legalize@xmission.com>
2014-12-02 03:20:26 -0700

    Correct compile error

--- hc/hc.cpp
+++ hc/hc.cpp
@@ -26,6 +26,8 @@
 #define FNSPLIT _splitpath
 
 #ifdef XFRACT
+#include <unistd.h>
+
 #ifndef HAVESTRI
 extern int stricmp(const char *, const char *);
 extern int strnicmp(const char *, const char *, int);
